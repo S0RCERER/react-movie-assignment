737d491739b3b85609b45b4ca659822850881466
Ling Feng <20099866@mail.wit.ie>
2022-11-15 01:39:24 +0000

    Added new contents to person details page.

--- movies/src/components/personDetails/index.js
+++ movies/src/components/personDetails/index.js
@@ -1,15 +1,42 @@
 import Typography from "@mui/material/Typography";
 import React from "react";
 
+const PersonDetails = ({ person }) => {  // Don't miss this!
 
-const PersonDetails = () => {  // Don't miss this!
-  return (
+    return (
     <>
-      <Typography variant="h5" component="h3">
+      <Typography variant="h" component="h3">
         Overview
       </Typography>
-
-      </>
+      <Typography variant="h6" component="p">
+        {person.biography}
+      </Typography>
+      <Typography variant="h" component="h3">
+        Popular Index
+      </Typography>
+      <Typography variant="h6" component="p">
+        {person.popularity}
+      </Typography>
+      <Typography variant="h" component="h3">
+        Birthday
+      </Typography>
+      <Typography variant="h6" component="p">
+        {person.birthday}
+      </Typography>
+      <Typography variant="h" component="h3">
+        Place Of Birth
+      </Typography>
+      <Typography variant="h6" component="p">
+        {person.place_of_birth}
+      </Typography>
+      <Typography variant="h" component="h3">
+        Also Known As
+      </Typography>
+      <Typography variant="h6" component="p">
+        {person.also_known_as}
+      </Typography>
+      
+    </>
   );
 };
 export default PersonDetails ;
